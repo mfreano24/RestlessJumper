4eceb66b7ccfae86a8a1bbb8481c2b10670eff8f
Michael Freaney <michael.freaney324@gmail.com>
2021-05-14 21:08:34 -0500

    initial commit

---
+++ src/GameObject.h
@@ -0,0 +1,67 @@
+
+#pragma once
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+const float GRAVITY = -9.81f;
+
+class GameObject{
+    public:
+    //transform
+    glm::vec3 position;
+    glm::vec3 rotation;
+    glm::vec3 scale;
+
+    GameObject(): position(glm::vec3(0)), rotation(glm::vec3(0)), scale(glm::vec3(1)) {}
+    GameObject(glm::vec3& _pos, glm::vec3& _rot, glm::vec3& _sca): position(_pos), rotation(_rot), scale(_sca) {}
+    ~GameObject();
+};
+
+
+class Collider : public GameObject{
+    //use this like a box collider
+    public:
+    float x_extent;
+    float y_extent;
+    bool isTrigger;
+    
+    Collider(): 
+    x_extent(1.0f),
+    y_extent(1.0f),
+    isTrigger(false),
+    GameObject()
+    {}
+
+    Collider(glm::vec3 &_pos, glm::vec3 &_rot, glm::vec3 &_sca, float &_x, float &_y, bool _t) :
+    x_extent(_x),
+    y_extent(_y), 
+    isTrigger(_t),
+    GameObject(_pos, _rot, _sca)
+    {}
+
+    ~Collider();
+
+    bool CheckCollision();
+};
+
+
+class RigidBody : public Collider{
+    public:
+    glm::vec3 velocity;
+
+    RigidBody():
+    velocity(glm::vec3(0)),
+    Collider()
+    {}
+
+    RigidBody(glm::vec3 &_pos, glm::vec3 &_rot, glm::vec3 &_sca, float &_x, float &_y, bool _t):
+    velocity(glm::vec3(0)),
+    Collider(_pos, _rot, _sca, _x, _y, _t)
+    {}
+
+    ~RigidBody();
+
+    void UpdatePosition();
+};
---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <cassert>
+#include <cstring>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "GameObject.h"
+
+using namespace std;
+using namespace glm;
+
+int main()
+{
+
+    cout << "Program runs just fine." << endl;
+
+}
