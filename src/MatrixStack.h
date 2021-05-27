15ef8b15293490d88c2304ac36effa2894ab50a3
Michael Freaney <michael.freaney324@gmail.com>
2021-05-26 19:30:41 -0500

    forgot to save MatrixStack.h

--- src/MatrixStack.h
+++ src/MatrixStack.h
@@ -14,6 +14,7 @@ class MatrixStack{
     void pushMatrix();
     void popMatrix();
 
+    void loadIdentity();
     void translate(glm::vec3& xyz);
     //rotate around some axis by angle degrees.
     void rotate(float angle, glm::vec3& axis);
