939d0cf14d64ed69701e6cde44d1405b1667d17c
Michael Freaney <michael.freaney324@gmail.com>
2021-05-26 19:29:59 -0500

    filled in matrix stack code, began very basic collision detection

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,10 +1,33 @@
 #include "GameObject.h"
+#include "Game.h"
 using namespace std;
 using namespace glm;
 
 
 void GameObject::Draw(/* MatrixStack* M */){
     //use the matrix stack and render the object here
+}
+
+bool Collider::CheckCollision(){
+    vector<Collider*> coll = Game::Instance().collision;
+    for(auto c : coll){
+        //this is a primitive version of our collision testing, where we assume one object is moving and one isn't
+        //this also excludes localized testing- if we use a tree later we won't be testing on everything every frame :)
+        if(c->name == this->name){
+            //don't check collision with ourselves, that'd always return true!
+            continue;
+        }
+        
+        //need top left positions
+        vec3 A = c->position - vec3(c->x_extent, c->y_extent, 0.0);
+        vec3 B = this->position - vec3(this->x_extent, this->y_extent, 0.0);
+
+        bool collisionX = A.x + 2 * c->x_extent >= B.x && B.x + 2 * this->x_extent >= A.x;
+        bool collisionY = A.y + 2 * c->y_extent >= B.y && B.y + 2 * this->y_extent >= A.y;
+        if(collisionX && collisionY){
+            return true;
+        }
+    }
 }
 
 void RigidBody::UpdatePosition(float deltatime){
--- src/MatrixStack.cpp
+++ src/MatrixStack.cpp
@@ -19,26 +19,42 @@ MatrixStack::MatrixStack(){
 
 MatrixStack::~MatrixStack(){}
 
+//TODO: ALL OF THIS
+
 void MatrixStack::pushMatrix(){
+    const mat4 &top = t->top();
+    t->push(top);
+    assert(t->size() <= 50); //dont overdo it, these multiplications take a while
 
 }
 
 void MatrixStack::popMatrix(){
+    assert(!t->empty());
+    t->pop();
+    assert(!t->empty()); //should always at least have the identity matrix on there.
+}
 
+void MatrixStack::loadIdentity(){
+    glm::mat4 &top = t->top(); //ampersand makes this a reference
+    top = glm::mat4(1.0);
 }
 
 void MatrixStack::translate(glm::vec3 &xyz){
-
+    glm::mat4 &top = t->top();
+    top *= glm::translate(mat4(1.0), xyz);
 }
+
 //rotate around some axis by angle degrees.
 void MatrixStack::rotate(float angle, glm::vec3 &axis){
-
+    glm::mat4 &top = t->top();
+    top *= glm::rotate(glm::mat4(1.0f), angle, axis);
 }
 
 void MatrixStack::scale(glm::vec3 &xyz){
-
+    glm::mat4 &top = t->top();
+    top *= glm::scale(glm::mat4(1.0f), xyz);
 }
 
-glm::mat4 MatrixStack::topMatrix() const{
-    
+mat4 MatrixStack::topMatrix() const{
+    return t->top();
 }
